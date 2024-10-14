25038497081fd93408dde452d842c063a9bb6325
Peter Bjorklund <piot@hotmail.com>
2024-10-14 15:08:21 +0200

    chore: initial commit

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# network-metrics
+# 📊 Network Metrics
+
+Welcome to the **Network Metrics** crate! This library provides a simple and efficient way to track and analyze network metrics, including datagrams and octets per second, in real-time.
+Whether you are building a game server, a network monitoring tool, or any application that requires network performance insights, this crate has you covered! 🚀
+
+## Features ✨
+
+- **Real-time Metrics**: Track incoming and outgoing datagrams and octets per second with minimal overhead.
+- **Display Support**: Easily convert metrics to human-readable strings.
+- **Customizable**: Adjust the update intervals and durations for metrics tracking.
+
+## Installation
+
+Add the following line to your `Cargo.toml` file:
+
+```toml
+[dependencies]
+network-metrics = "0.0.1"
+```
---
+++ src/lib.rs
@@ -0,0 +1,116 @@
+/*
+ * Copyright (c) Peter Bjorklund. All rights reserved. https://github.com/piot/network-metrics
+ * Licensed under the MIT License. See LICENSE in the project root for license information.
+ */
+
+use log::debug;
+use metricator::RateMetric;
+use monotonic_time_rs::{Millis, MillisDuration};
+use std::fmt::Display;
+
+pub struct MetricsInDirection {
+    pub datagrams_per_second: f32,
+    pub octets_per_second: f32,
+}
+
+impl Display for MetricsInDirection {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{} datagrams/s {} octets/s",
+            self.datagrams_per_second, self.octets_per_second
+        )
+    }
+}
+
+pub struct CombinedMetrics {
+    pub outgoing: MetricsInDirection,
+    pub incoming: MetricsInDirection,
+}
+
+impl Display for CombinedMetrics {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "metrics: out:\n{}, in:\n{}",
+            self.outgoing, self.incoming
+        )
+    }
+}
+
+pub struct NetworkMetrics {
+    in_datagrams_per_second: RateMetric,
+    in_octets_per_second: RateMetric,
+    out_datagrams_per_second: RateMetric,
+    out_octets_per_second: RateMetric,
+
+    last_debug_metric_at: Millis,
+    debug_metric_duration: MillisDuration,
+}
+
+impl NetworkMetrics {
+    pub fn new(now: Millis) -> Self {
+        Self {
+            in_datagrams_per_second: RateMetric::with_interval(now, 0.1),
+            in_octets_per_second: RateMetric::with_interval(now, 0.1),
+
+            out_datagrams_per_second: RateMetric::with_interval(now, 0.1),
+            out_octets_per_second: RateMetric::with_interval(now, 0.1),
+            last_debug_metric_at: now,
+            debug_metric_duration: MillisDuration::from_millis(500),
+        }
+    }
+
+    pub fn sent_datagrams(&mut self, datagrams: &Vec<Vec<u8>>) {
+        for datagram in datagrams {
+            self.out_octets_per_second.add(datagram.len() as u32)
+        }
+        self.out_datagrams_per_second.add(datagrams.len() as u32);
+    }
+
+    pub fn received_datagram(&mut self, datagram: &[u8]) {
+        self.in_octets_per_second.add(datagram.len() as u32);
+        self.in_datagrams_per_second.add(1);
+    }
+
+    pub fn update_metrics(&mut self, now: Millis) {
+        self.in_datagrams_per_second.update(now);
+        self.in_octets_per_second.update(now);
+        self.out_datagrams_per_second.update(now);
+        self.out_octets_per_second.update(now);
+
+        if now - self.last_debug_metric_at > self.debug_metric_duration {
+            self.last_debug_metric_at = now;
+            debug!("metrics: {}", self.metrics())
+        }
+    }
+
+    pub fn metrics(&self) -> CombinedMetrics {
+        CombinedMetrics {
+            outgoing: MetricsInDirection {
+                datagrams_per_second: self.out_datagrams_per_second.rate(),
+                octets_per_second: self.out_octets_per_second.rate(),
+            },
+            incoming: MetricsInDirection {
+                datagrams_per_second: self.in_datagrams_per_second.rate(),
+                octets_per_second: self.in_octets_per_second.rate(),
+            },
+        }
+    }
+
+    pub fn in_datagrams_per_second(&self) -> f32 {
+        self.in_datagrams_per_second.rate()
+    }
+
+    pub fn in_octets_per_second(&self) -> f32 {
+        self.in_octets_per_second.rate()
+    }
+
+    pub fn out_datagrams_per_second(&self) -> f32 {
+        self.out_datagrams_per_second.rate()
+    }
+
+    pub fn out_octets_per_second(&self) -> f32 {
+        self.out_octets_per_second.rate()
+    }
+}
